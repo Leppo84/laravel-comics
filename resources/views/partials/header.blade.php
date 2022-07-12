3bcc5d6da62ee3c7e2a3d7ab1427b554d3cf169e
LeppoXena <francesco.lepore@xenagames.com>
2022-07-12 19:58:20 +0200

    header added images added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,22 +1,19 @@
-<template>
-  <header class="bg white">
-    <div class="container fx jc-between ai-center">
-      <a href="#">
-        <img id="logo-dc" src="../assets/img/dc-logo.png" alt="Logo DC Home">
-      </a>
-      <nav>
-        <ul>
-          <li v-for="(link, index) in links" :key="index" :class="link.current?'active':''">
-            <a :href="link.url">
-              link
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <div class="jumbotron">
-      <img src="../assets/img/jumbotron.jpg" alt="Awesome comics!">
-    </div>
-  </header>
-</template>
-
+<header class="bg white">
+  <div class="container fx jc-between ai-center">
+    <a href="#">
+      <img id="logo-dc" src="/img/dc-logo.png" alt="Logo DC Home">
+    </a>
+    <nav>
+      <ul>
+        <li class="">
+          <a :href="link.url">
+            link
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  <div class="jumbotron">
+    <img src="/img/jumbotron.jpg" alt="Awesome comics!">
+  </div>
+</header>
