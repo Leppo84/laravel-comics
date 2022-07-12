1d019999b892963147c09d8d622352fecd6bb945
LeppoXena <francesco.lepore@xenagames.com>
2022-07-12 18:11:55 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.base')
+
+@section('pageTitle','DC Comic')
+
+@endsection
---
+++ resources/views/partials/header.blades.php
@@ -0,0 +1,22 @@
+<template>
+  <header class="bg white">
+    <div class="container fx jc-between ai-center">
+      <a href="#">
+        <img id="logo-dc" src="../assets/img/dc-logo.png" alt="Logo DC Home">
+      </a>
+      <nav>
+        <ul>
+          <li v-for="(link, index) in links" :key="index" :class="link.current?'active':''">
+            <a :href="link.url">
+              <!-- {{link.text}} -->
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div class="jumbotron">
+      <img src="../assets/img/jumbotron.jpg" alt="Awesome comics!">
+    </div>
+  </header>
+</template>
+
